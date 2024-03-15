461d6bfd1465f710599d16a756e1926d52c4f116
janlin002 <123davidbill@gmail.com>
2024-03-15 16:07:36 +0800

    feat: add useRequest

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { useTheme, Theme } from './hooks/useTheme'
 import useBattery from './hooks/useBattery'
 import useHover from './hooks/useHover'
+import useRequest from './hooks/useRequest'
 
 // firebase
 import useRemoteConfig from './hooks/firebase/useRemoteConfig'
 
-export { useTheme, Theme, useBattery, useHover, useRemoteConfig }
+export { useTheme, Theme, useBattery, useHover, useRequest, useRemoteConfig }
