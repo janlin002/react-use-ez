e4bf885cdf9dafc3bb8b29c0e2d4d220c0687be7
janlin002 <123davidbill@gmail.com>
2024-03-19 22:23:09 +0800

    feat: add useFactorial

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,9 @@ import useBattery from './hooks/useBattery'
 import useHover from './hooks/useHover'
 import useRequest from './hooks/useRequest'
 import useCookie from './hooks/useCookie'
+import useFactorial from './hooks/useFactorial'
 
 // firebase
 import useRemoteConfig from './hooks/firebase/useRemoteConfig'
 
-export { useTheme, Theme, useBattery, useHover, useRequest, useCookie, useRemoteConfig }
+export { useTheme, Theme, useBattery, useHover, useRemoteConfig, useRequest, useCookie, useFactorial }
